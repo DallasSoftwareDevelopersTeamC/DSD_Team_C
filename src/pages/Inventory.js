bc08e23baac81d1785b88ae2941e928257be43a0
Joshua Ow <joshuaow@gmail.com>
2023-03-02 09:50:57 -0600

    fix: added correct directory link for inventory.js

--- src/pages/Inventory.js
+++ src/pages/Inventory.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import Inventory from '../components/InventoryContent.js';
+import Inventory from '../components/Inventory/InventoryContent.js';
 
 function Inventory() {
   return (
