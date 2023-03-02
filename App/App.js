e8acbe1edc31be666b29423c92bbb07b108f93db
Joshua Ow <joshuaow@gmail.com>
2023-03-01 20:04:56 -0600

    Added React Files for FrontEnd - Testing structure

---
+++ App/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Header from './components/index/Header.js'
+
+export default function App(){
+    return (
+        <div>
+            <Header />
+        </div>
+    )
+}
---
+++ App/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import React from 'react-dom/client'
+import App from './App'
+
+ReactDOM.createRoot(<App />, document.getElementById("root")).render()
