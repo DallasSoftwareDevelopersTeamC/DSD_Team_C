fa78f4a64132b3e79d0b3cb389f3f54c2573b6c5
Joshua Ow <joshuaow@gmail.com>
2023-03-02 09:41:52 -0600

    final structure, removed tailwind

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import React from 'react-dom/client'
-import './tailwind.css'
 import App from './App'
 
 ReactDOM.createRoot(<App />, document.getElementById("root")).render()
