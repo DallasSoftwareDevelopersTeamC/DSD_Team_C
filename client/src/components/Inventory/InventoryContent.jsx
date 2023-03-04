a69cf03df752aa6c0d8f2603f2c6467425a87ffb
Joshua Ow <joshuaow@gmail.com>
2023-03-04 09:53:11 -0600

    Feat: added inventory items table/cells. Included media queries to make responsive on ipad and mobile. created separate components for the filter by row and inventory items row and imported that into the inventorypage component.

--- client/src/components/Inventory/InventoryContent.jsx
+++ client/src/components/Inventory/InventoryContent.jsx
@@ -1,30 +1,12 @@
 import React from 'react';
+import InventoryFilterRow from './InventoryFilSeaAdd';
+import InventoryItems from './InventoryItems';
 
 function InventoryContent() {
   return (
-    <div className="inventory-container">
-      <div className="inventory-row">
-        <div className="inventory-filter-by">
-          <select className='filter-style'>
-            <option label='Filter By'></option>
-            <option value="sku">SKU</option>
-            <option value="image">Image</option>
-            <option value="brand">Brand</option>
-            <option value="name">Name</option>
-            <option value="in-stock">In Stock</option>
-            <option value="reorder-at">Reorder At</option>
-            <option value="order-qty">Order Qty</option>
-            <option value="qty">Qty</option>
-            <option value="arrival">Arrival</option>
-          </select>
-        </div>
-        <div className="inventory-search">
-          <input type="text" placeholder="Search" className='search-input'/>
-        </div>
-        <div className="inventory-add-product">
-          <button className='add-prod-btn'>Add Product</button>
-        </div>
-      </div>
+    <div>
+      <InventoryFilterRow />
+      <InventoryItems />
     </div>
   );
 }
---
+++ server/src/server 3.js
@@ -0,0 +1,40 @@
+'use strict'
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod }
+  }
+Object.defineProperty(exports, '__esModule', { value: true })
+
+// was having problems using this express syntax with the (app.use(express.json())) below
+// const express_1 = __importDefault(require('express'))
+// const app = (0, express_1.default)()  
+const express = require('express')
+const app = express()
+require('dotenv').config({ path: './config/.env' })
+const cors = require('cors')
+
+const inventoryRoutes = require('./routes/inventoryRoutes')
+const ordersRoutes = require('./routes/ordersRoutes')
+
+// allow all origins during development?
+app.use(
+  cors({
+    origin: '*',
+  })
+)
+// allowing express to read incoming json data
+app.use(express.json())
+// allowing express to read urlencoded data
+app.use(express.urlencoded({ extended: true }))
+app.use((req, res, next) => {
+  res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate')
+  next()
+})
+
+app.use('/', inventoryRoutes)
+app.use('/orders', ordersRoutes)
+
+app.listen(7777, () => {
+  console.log('server is running!')
+})
