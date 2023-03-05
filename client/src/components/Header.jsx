eb2fe24ff7c9ae865f087f0c4b5dc4a800ea0a8e
Joshua Ow <joshuaow@gmail.com>
2023-03-05 14:43:54 -0600

    fixed back to old logo

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,7 +5,7 @@ function Header(){
     return (
         <div className='header-nav'>
             <nav>
-                <a href="#"><img src="./src/assets/logo.png" alt="logo" className='logo'/></a>
+                <h1 className='header-name'><a href="#">Orderly</a></h1>
                 <ul className='nav-links'>
                     <li><a href="/" className='header-li-a'>Inventory</a></li>
                     <li><a href="/Orders" className='header-li-a'>Orders</a></li>
