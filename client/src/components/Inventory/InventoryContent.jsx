d001107113f1607c2a577e828870f9bb4ab4687a
WallaceRepo <sophiwebhub@gmail.com>
2023-03-04 21:15:02 -0600

    Created mock data.json file. Created react-context in inventory jsx

--- client/src/components/Inventory/InventoryContent.jsx
+++ client/src/components/Inventory/InventoryContent.jsx
@@ -2,11 +2,34 @@ import React from 'react';
 import InventoryFilterRow from './InventoryFilSeaAdd';
 import InventoryItems from './InventoryItems';
 
-function InventoryContent() {
-  return (
+import {useContext, useState} from 'react';
+
+import { InventoryContext } from '../../contexts/inventory.context';
+
+ const InventoryContent = () => {
+   const {inventory} = useContext(InventoryContext)
+     // console.log(inventory)
+    return (
     <div>
       <InventoryFilterRow />
       <InventoryItems />
+      <div className="table-row">
+       {inventory.map((item) => (
+        <div key = {item.id}>    
+         <div className="table-cell">{item.sku}</div>
+        <img className="" src={item.imageUrl}/>
+        <div className="table-cell">{item.brand}</div>
+        <div className="table-cell">{item.name}</div>
+        <div className="table-cell">{item.inStock}</div>
+        <div className="table-cell">{item.reOrderAt}</div>
+        <div className="table-cell">{item.set_qty_order}</div>
+        <div className="table-cell">{item.incoming_qty}</div>
+        <div className="table-cell">{item.arrival}</div>
+        <div className="table-cell"><button>Order</button></div>
+        <div className="table-cell"><button>Settings</button></div>
+      </div>
+     ))}
+    </div>
     </div>
   );
 }
