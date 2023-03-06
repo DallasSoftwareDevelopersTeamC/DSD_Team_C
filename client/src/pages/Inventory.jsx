82665afa6c7e3ee8c2b05607914a996432c61d00
WallaceRepo <sophiwebhub@gmail.com>
2023-03-05 22:45:10 -0600

    Created state to control incoming, order, settings btns to open and close

--- client/src/pages/Inventory.jsx
+++ client/src/pages/Inventory.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from '../components/Header.jsx';
 import Footer from '../components/Footer.jsx';
-import InventoryContent from '../components/Inventory/InventoryContent.jsx';
+import InventoryContent from '../components/Inventory/Inventory.jsx';
 
 function InventoryPage() {
   return (
