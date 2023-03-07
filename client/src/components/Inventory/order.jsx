b41cb817891016fc91785d861b7e043dd8f02708
Joshua Ow <joshuaow@gmail.com>
2023-03-06 20:17:11 -0600

    FIX: Refactored inventory.jsx, now using tables - less classes/divs. refactored one pop up component. will finish this tomorrow

--- client/src/components/Inventory/order.jsx
+++ client/src/components/Inventory/order.jsx
@@ -1,38 +1,41 @@
 import React from 'react';
 
-
 export default function Order({ handleClick, popup}) {
-  return (<div className="popup">
-           <div className='popup-cont'>
-                <div className="popup-row-header">
-                    <div className="popup-cell">Product</div>
-                    <div className="popup-cell">Vendor</div>
-                    <div className="popup-cell">Shipper</div>
-                    <div className="popup-cell">Name</div>
-                    <div className="popup-cell">Price EA</div>
-                    <div className="popup-cell">Shipping Cost</div>
-                    <div className="popup-cell">Total</div>
-                </div>
-                <div className="popup-row-right">
-                    <div className="popup-cell">Apple Watch</div>
-                    <div className="popup-cell">Date</div>
-                    <div className="popup-cell">Apple</div>
-                    <div className="popup-cell">Name Here</div>
-                    <div className="popup-cell">$4546</div>
-                </div>
-                <div className="popup-row-right">
-                    <div className="popup-cell">-</div>
-                    <div className="popup-cell">QTY</div>
-                    <div className="popup-cell">Apple</div>
-                    <div className="popup-cell">Name Here</div>
-                    <div className="popup-cell">$4546</div>
-                </div>
-           </div>
-                <div className='btn-cont'>
-                    <button className="pop-btn-order">View more</button>
-                    <button id ="close" onClick={(event)=>handleClick(event)} className= { popup == "close" ? "hide":"show"}>Close</button>
-                </div>
-         
-     </div>
+  return (
+    <div className="popup">
+      <table className='popup-cont'>
+        <thead className='popup-row-header'>
+            <td>Product</td>
+            <td>Vendor</td>
+            <td>Shipper</td>
+            <td>Name</td>
+            <td>Price EA</td>
+            <td>Shipping Cost</td>
+            <td>Total</td>
+        </thead>
+        <tbody className='body'>
+            <td>Apple Watch</td>
+            <td>Date</td>
+            <td>Apple</td>
+            <td>Name Here</td>
+            <td>$4546</td>
+            <td>-</td>
+            <td>$4546</td>
+        </tbody>
+        <tbody className='body'>
+            <td>-</td>
+            <td>QTY</td>
+            <td>Apple</td>
+            <td>Name Here</td>
+            <td>$4546</td>
+            <td>-</td>
+            <td>$4546</td>
+        </tbody>
+      </table>
+      <tbody className='btn-cont'>
+          <td><button className="pop-button">Order Now</button></td>
+          <td><button id="close" onClick={(event)=>handleClick(event)} className={popup === "close" ? "hide" : "show"}>Close</button></td>
+      </tbody>
+    </div>
   )
 }
