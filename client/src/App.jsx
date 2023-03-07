a9a0ffd3b9dc96f667f43d40a199d9771f291e8e
Joshua Ow <joshuaow@gmail.com>
2023-03-07 17:20:05 -0600

    fix: input borders fixed. Feat: replaced buttons with icons. issue - icon popups work in specific area of icon, need to fix.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,8 +2,11 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import InventoryPage from './pages/Inventory.jsx';
 import SettingsPage from './pages/Settings.jsx';
 import OrdersPage from './pages/Orders.jsx';
-
 import { InventoryProvider } from './contexts/inventory.context.jsx';
+import { faBox, faGear, faBagShopping} from '@fortawesome/free-solid-svg-icons';
+// import { fab } from '@fortawesome/free-brands-svg-icons' this is if we decide to use any branded icons
+import { library } from '@fortawesome/fontawesome-svg-core';
+library.add(faBox, faGear, faBagShopping)
 
 function App() {
   return (
