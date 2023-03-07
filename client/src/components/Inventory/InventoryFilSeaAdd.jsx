116150cf172c715807d334bef0adefcdd646a8b4
Joshua Ow <joshuaow@gmail.com>
2023-03-07 10:18:41 -0600

    FIX: all popup components utilizing table and more readable classes. filter by/search/add row now utilizing tables and less css for better reabability.

--- client/src/components/Inventory/InventoryFilSeaAdd.jsx
+++ client/src/components/Inventory/InventoryFilSeaAdd.jsx
@@ -2,30 +2,30 @@ import React from "react";
 
 function InventoryFilterRow (){
     return (
-        <div className="inventory-container">
-        <div className="inventory-row">
-          <div className="inventory-filter-by">
-            <select className='filter-style'>
-              <option label='Filter By'></option>
-              <option value="sku">SKU</option>
-              <option value="image">Image</option>
-              <option value="brand">Brand</option>
-              <option value="name">Name</option>
-              <option value="in-stock">In Stock</option>
-              <option value="reorder-at">Reorder At</option>
-              <option value="order-qty">Order Qty</option>
-              <option value="qty">Qty</option>
-              <option value="arrival">Arrival</option>
-            </select>
-          </div>
-          <div className="inventory-search">
-            <input type="text" placeholder="Search" className='search-input'/>
-          </div>
-          <div className="inventory-add-product">
+        <table className="table-filter-search-add">
+          <tr className="tr-filter-search-add">
+            <td>
+              <select className='filter-style'>
+                <option label='Filter By'></option>
+                <option value="sku">SKU</option>
+                <option value="image">Image</option>
+                <option value="brand">Brand</option>
+                <option value="name">Name</option>
+                <option value="in-stock">In Stock</option>
+                <option value="reorder-at">Reorder At</option>
+                <option value="order-qty">Order Qty</option>
+                <option value="qty">Qty</option>
+                <option value="arrival">Arrival</option>
+              </select>
+            </td>
+            <td>
+              <input type="text" placeholder="Search" className='search-input'/>
+            </td>
+            <td>
             <button className='add-prod-btn'>Add Product</button>
-          </div>
-        </div>
-      </div>
+            </td>
+          </tr>
+        </table>
     )
 }
 
