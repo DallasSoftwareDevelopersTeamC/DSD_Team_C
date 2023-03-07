34a59489c960ab10499c814598f991c91f611c59
Breland <claybreland1@gmail.com>
2023-03-07 11:41:50 -0600

    setup: inventory list now loads from API call to backend

--- client/src/components/Inventory/InventoryFilSeaAdd.jsx
+++ client/src/components/Inventory/InventoryFilSeaAdd.jsx
@@ -1,32 +1,32 @@
 import React from "react";
 
-function InventoryFilterRow (){
-    return (
-        <div className="inventory-container">
-        <div className="inventory-row">
-          <div className="inventory-filter-by">
-            <select className='filter-style'>
-              <option label='Filter By'></option>
-              <option value="sku">SKU</option>
-              <option value="image">Image</option>
-              <option value="brand">Brand</option>
-              <option value="name">Name</option>
-              <option value="in-stock">In Stock</option>
-              <option value="reorder-at">Reorder At</option>
-              <option value="order-qty">Order Qty</option>
-              <option value="qty">Qty</option>
-              <option value="arrival">Arrival</option>
-            </select>
-          </div>
-          <div className="inventory-search">
-            <input type="text" placeholder="Search" className='search-input'/>
-          </div>
-          <div className="inventory-add-product">
-            <button className='add-prod-btn'>Add Product</button>
-          </div>
+function InventoryFilterRow() {
+  return (
+    <div className="inventory-container">
+      <div className="inventory-row">
+        <div className="inventory-filter-by">
+          <select className='filter-style'>
+            <option label='Filter By'></option>
+            <option value="sku">SKU</option>
+            <option value="image">Image</option>
+            <option value="brand">Brand</option>
+            <option value="name">Name</option>
+            <option value="in-stock">In Stock</option>
+            <option value="reorder-at">Reorder At</option>
+            <option value="order-qty">Order Qty</option>
+            <option value="qty">Qty</option>
+            <option value="arrival">Arrival</option>
+          </select>
+        </div>
+        <div className="inventory-search">
+          <input type="text" placeholder="Search" className='search-input' />
+        </div>
+        <div className="inventory-add-product">
+          <button className='add-prod-btn'>Add Product</button>
         </div>
       </div>
-    )
+    </div>
+  )
 }
 
 export default InventoryFilterRow
--- client/src/contexts/inventory.context.jsx
+++ client/src/contexts/inventory.context.jsx
@@ -1,15 +1,24 @@
-import { createContext, useState } from "react";
-import INVENTORY from '../assets/data.json';
+import { createContext, useState, useEffect } from "react";
+import { getInventoryList } from '../services/inventoryAPIcalls';
 
-export const InventoryContext =  createContext({
-    inventory: []
+export const InventoryContext = createContext({
+  inventory: []
 
 });
 
-export const InventoryProvider =( {children}) => {
-    const [inventory, setInventory] = useState(INVENTORY);
-    const value = {inventory}
+export const InventoryProvider = ({ children }) => {
+  // set inventory to empty array initially
+  const [inventory, setInventory] = useState([]);
+  // then pull in the data from api call
+  useEffect(() => {
+    getInventoryList().then((data) => {
+      setInventory(data);
+    }).catch((error) => {
+      console.error('Error fetching inventory list:', error);
+    });
+  }, []);
+  const value = { inventory }
   return (
-     <InventoryContext.Provider value={value}>{ children }</InventoryContext.Provider>
+    <InventoryContext.Provider value={value}>{children}</InventoryContext.Provider>
   )
 }
