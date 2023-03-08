66b2a26c1a50c6b20fd377fd06adb22936cbb16d
Ej1seven <erikhunter@erikhunter.dev>
2023-03-08 09:05:16 -0600

    updated the createInventoryItem() in the inventoryAPIcalls.js file

--- client/src/services/inventoryAPIcalls.js
+++ client/src/services/inventoryAPIcalls.js
@@ -1,56 +1,68 @@
 import { API_URL } from './config';
 
-
 export async function getInventoryList() {
-    const response = await fetch(`${API_URL}/`, {
-        method: 'GET'
-    });
-    return response.json();
+  const response = await fetch(`${API_URL}/`, {
+    method: 'GET',
+  });
+  return response.json();
 }
 
-
 export async function getInventoryItem(id) {
-    const response = await fetch(`${API_URL}/${id}`, {
-        method: 'GET'
-    });
-    return response.json();
+  const response = await fetch(`${API_URL}/${id}`, {
+    method: 'GET',
+  });
+  return response.json();
 }
 
 // totalIncomingQty, incomingDates,
-export async function createInventoryItem(sku, brand, name, description, inStock, reorderAt, orderQTY, shipper, priceEA, orderNowOrderQty, shippingCost, itemUsageSpeed) {
-    const response = await fetch(`${API_URL}/`, {
-        method: 'POST',
-        body: JSON.stringify({
-            sku, brand, name, description, inStock, reorderAt, orderQTY, shipper, priceEA, orderNowOrderQty, shippingCost, itemUsageSpeed
-        }),
-        headers: {
-            'Content-Type': 'application/json'
-        },
-    });
-    return response.json();
+export async function createInventoryItem(
+  sku,
+  brand,
+  productName,
+  description,
+  inStock,
+  reorderAt,
+  orderQty,
+  totalIncomingQty
+) {
+  const response = await fetch(`${API_URL}/`, {
+    method: 'POST',
+    body: JSON.stringify({
+      sku,
+      brand,
+      productName,
+      description,
+      inStock,
+      reorderAt,
+      orderQty,
+      totalIncomingQty,
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  return response.json();
 }
-
 
 export async function updateInventoryItem(id, updates) {
-    const response = await fetch(`${API_URL}/${id}`, {
-        method: 'PATCH',
-        body: JSON.stringify({
-            updates
-        }),
-        headers: {
-            'Content-Type': 'application/json'
-        },
-    });
-    return response.json();
+  const response = await fetch(`${API_URL}/${id}`, {
+    method: 'PATCH',
+    body: JSON.stringify({
+      updates,
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  return response.json();
 }
 
-
 export async function deleteInventoryItem(id) {
-    const response = await fetch(`${API_URL}/${id}`, {
-        method: 'DELETE',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-    });
-    return response.json();
+  const response = await fetch(`${API_URL}/${id}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  return response.json();
 }
