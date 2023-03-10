4fea9ea15bffa2b04aee25047725c1159a21e052
Ej1seven <erikhunter@erikhunter.dev>
2023-03-10 02:28:50 -0600

    convert csv file into json

--- server/src/routes/inventoryRoutes.js
+++ server/src/routes/inventoryRoutes.js
@@ -1,20 +1,28 @@
-const multer = require('multer');
-const upload = multer({
-  storage: storage,
-  fileFilter: (req, file, cb) => {
-    if (file.mimetype == 'text/csv') {
-      cb(null, true);
-    } else {
-      cb(null, false);
-      return cb(new Error('Only .csv format allowed!'));
-    }
-  },
-});
 // const authController = require('../controllers/')
-const inventoryController = require('../controllers/inventory');
 const express = require('express');
 const inventory = require('../controllers/inventory');
-const router = express.Router();
+const router = express();
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, 'uploads');
+//   },
+//   filename: function (req, file, cb) {
+//     console.log(file);
+//     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+//     cb(null, Date.now() + path.extname(file.originalname));
+//   },
+// });
+// const upload = multer({
+//   storage: storage,
+//   fileFilter: (req, file, cb) => {
+//     if (file.mimetype == 'text/csv') {
+//       cb(null, true);
+//     } else {
+//       cb(null, false);
+//       return cb(new Error('Only .csv format allowed!'));
+//     }
+//   },
+// });
 // const authController = require('../controllers/')
 const inventoryController = require('../controllers/inventory');
 
@@ -22,11 +30,7 @@ router.get('/', inventoryController.getInventoryList);
 router.get('/:id', inventoryController.getInventoryItem);
 
 router.post('/', inventoryController.createInventoryItem);
-router.post(
-  '/upload',
-  upload.single('myFile'),
-  inventoryController.convertCsvFileToJson
-);
+router.post('/upload', inventoryController.convertCsvFileToJson);
 
 // we can use PATCH to replce some values or use PUT to replace whole item
 router.patch('/:id', inventoryController.updateInventoryItem);
