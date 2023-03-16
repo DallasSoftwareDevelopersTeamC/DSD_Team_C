cdb10ffd60acebc8d1086f2099848c6a8ee37602
Ej1seven <erikhunter@erikhunter.dev>
2023-03-16 00:51:03 -0500

    created login page

--- client/src/components/Login/LoginWindow.jsx
+++ client/src/components/Login/LoginWindow.jsx
@@ -1,4 +1,7 @@
 import './loginWindow.css';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import { styled } from '@mui/material/styles';
 
 export default function () {
   return (
@@ -6,7 +9,36 @@ export default function () {
       <div className="title-container">
         <h1 className="title">Orderly</h1>
       </div>
-      <div className="input-container"></div>
+      <div className="input-container">
+        {/* <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}> */}{' '}
+        <p className="input-header">Login</p>
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          id="username"
+          label="Username"
+          name="username"
+          autoComplete="username"
+          autoFocus
+          InputLabelProps={{
+            style: { color: '#3b9893' },
+          }}
+        />
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          name="password"
+          label="Password"
+          type="password"
+          id="password"
+          autoComplete="current-password"
+          InputLabelProps={{
+            style: { color: '#3b9893' },
+          }}
+        />
+      </div>
     </div>
   );
 }
