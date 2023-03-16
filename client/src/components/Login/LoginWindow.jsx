1322dd574d7682b0bf0751ad4da646e0e7bcb31f
Ej1seven <erikhunter@erikhunter.dev>
2023-03-15 23:45:36 -0500

    added login page

---
+++ client/src/components/Login/LoginWindow.jsx
@@ -0,0 +1,12 @@
+import './loginWindow.css';
+
+export default function () {
+  return (
+    <div className="loginWindow-container">
+      <div className="title-container">
+        <h1 className="title">Orderly</h1>
+      </div>
+      <div className="input-container"></div>
+    </div>
+  );
+}
---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './login.css';
+import LoginWindow from '../components/Login/LoginWindow.jsx';
+
+function LoginPage() {
+  return (
+    <div className="login-container">
+      <LoginWindow />
+    </div>
+  );
+}
+
+export default LoginPage;
