d98c77fa9d3ddc3e75a082984fda7e509d51613a
Breland <claybreland1@gmail.com>
2023-03-17 18:22:42 -0500

    inventory context provider moved to main.jsx to wrap App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,15 +3,8 @@ import InventoryPage from './pages/InventoryPage.jsx';
 import SettingsPage from './pages/SettingsPage.jsx';
 import OrdersPage from './pages/OrdersPage.jsx';
 import LoginPage from './pages/LoginPage.jsx';
-import { InventoryProvider } from './contexts/inventory.context.jsx';
-import { OrdersProvider } from './contexts/orders.context.jsx';
-import {
-  faBox,
-  faGear,
-  faBagShopping,
-  faMagnifyingGlass,
-  faCircleXmark,
-} from '@fortawesome/free-solid-svg-icons';
+
+import { faBox, faGear, faBagShopping, faMagnifyingGlass, faCircleXmark, } from '@fortawesome/free-solid-svg-icons';
 // import { fab } from '@fortawesome/free-brands-svg-icons' this is if we decide to use any branded icons
 import { library } from '@fortawesome/fontawesome-svg-core';
 library.add(faBox, faGear, faBagShopping, faMagnifyingGlass, faCircleXmark);
@@ -19,16 +12,14 @@ library.add(faBox, faGear, faBagShopping, faMagnifyingGlass, faCircleXmark);
 function App() {
   return (
     <Router>
-      <InventoryProvider>
-        <OrdersProvider>
-          <Routes>
-            <Route path="/" element={<InventoryPage />} />
-            <Route path="/settings" element={<SettingsPage />} />
-            <Route path="/orders" element={<OrdersPage />} />
-            <Route path="/login" element={<LoginPage />} />
-          </Routes>
-        </OrdersProvider>
-      </InventoryProvider>
+
+      <Routes>
+        <Route path="/" element={<InventoryPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/orders" element={<OrdersPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+
     </Router>
   );
 }
