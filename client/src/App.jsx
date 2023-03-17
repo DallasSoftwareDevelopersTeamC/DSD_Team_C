8214e735d50168ad2a2c3502c889f9e4e6792322
Breland <claybreland1@gmail.com>
2023-03-17 18:44:01 -0500

    demo controls moved to App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import InventoryPage from './pages/InventoryPage.jsx';
 import SettingsPage from './pages/SettingsPage.jsx';
 import OrdersPage from './pages/OrdersPage.jsx';
 import LoginPage from './pages/LoginPage.jsx';
+import DemoControls from './components/DemoControls.jsx';
 import { useTempInStock } from './hooks/useTempStock';
 
 import { faBox, faGear, faBagShopping, faMagnifyingGlass, faCircleXmark, } from '@fortawesome/free-solid-svg-icons';
@@ -14,6 +15,7 @@ library.add(faBox, faGear, faBagShopping, faMagnifyingGlass, faCircleXmark);
 
 function App() {
   const { inventory, isUsingStock } = useContext(InventoryContext);
+
 
   // tempInStock decreases each inStock amount - the object is passed down as prop all the way to inventory.jsx 
   // where it uses this value to display the stock level for each item
@@ -28,6 +30,7 @@ function App() {
         <Route path="/orders" element={<OrdersPage />} />
         <Route path="/login" element={<LoginPage />} />
       </Routes>
+      <DemoControls />
     </Router>
   );
 }
--- client/src/pages/InventoryPage.jsx
+++ client/src/pages/InventoryPage.jsx
@@ -1,18 +1,15 @@
 import React, { useContext } from 'react';
 import Header from '../components/Header/Header.jsx';
-import DemoControls from '../components/Demo.jsx';
+
 import Footer from '../components/Footer.jsx';
 import InventoryContent from '../components/Inventory/Inventory.jsx';
-import { OrdersContext } from "../contexts/orders.context";
-
 
 function InventoryPage({ tempInStock }) {
-  const { deliveryState, setDeliveryState } = useContext(OrdersContext);
+
   return (
     <div>
       <Header />
       <InventoryContent tempInStock={tempInStock} />
-      <DemoControls />
       <Footer />
     </div>
   );
