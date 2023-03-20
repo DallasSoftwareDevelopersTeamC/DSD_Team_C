d7896a7bca0ae8f93c76ab9447243faf9b57f540
Joshua Ow <joshuaow@gmail.com>
2023-03-19 21:41:22 -0500

    added unique key prop to active orders to remedy the console log error

--- client/src/components/Orders/OrdersPreview.jsx
+++ client/src/components/Orders/OrdersPreview.jsx
@@ -72,7 +72,13 @@ function OrdersPreview() {
                         {Array.isArray(orders) && activeOrders.map((item, index) => (
                             // use key here to get specific item to get (for popup) update or delete. 
                             // item.sku value - this will scroll to selected value from searchInput.jsx
-                            <tr>
+                            <tr key={index}> 
+                            {/* the key above takes away the console log error. 
+                            a unique key prop is necessary to map over an array to create multiple elements, 
+                            each element should have a unique key to help React optimize rendering
+                            index used for now, However, using the index as a key is not recommended if your list can change 
+                            (e.g., items can be added, removed, or reordered) because it can lead to performance issues and incorrect behavior. 
+                            */}
 
                                 <td className="order-preview-sku">
                                     {item.SKU}
