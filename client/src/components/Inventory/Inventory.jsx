d847be392e88458b4d10fdd9c8423472458f59da
Ej1seven <erikhunter@erikhunter.dev>
2023-03-23 12:24:49 -0500

    added react hot toast icon

--- client/src/components/Inventory/Inventory.jsx
+++ client/src/components/Inventory/Inventory.jsx
@@ -24,6 +24,7 @@ import { authenticateUser } from '../../services/authenticationAPIcalls';
 import { useQuery } from 'react-query';
 import DropDownIcon from './popups/AddProductButton.jsx';
 import ScaleLoader from 'react-spinners/ScaleLoader';
+import toast, { Toaster } from 'react-hot-toast';
 
 export default function Inventory({ tempInStock }) {
   const { inventory, reloadInventory, isUsingStock } =
@@ -45,6 +46,15 @@ export default function Inventory({ tempInStock }) {
   useEffect(() => {
     if (isError) {
       alert(isError);
+    } else {
+      if (data?.username) {
+        toast.success(`Welcome back ${data?.username}`, {
+          style: {
+            background: '#333',
+            color: '#fff',
+          },
+        });
+      }
     }
   }, [data]);
 
@@ -229,8 +239,21 @@ export default function Inventory({ tempInStock }) {
 
   // ----------------------------------------------------------
   return (
-    <div className="headings-and-table-container" id="inventory">
-      {/* {isLoading ? (
+    <>
+      <Toaster
+        position="top-center"
+        reverseOrder={false}
+        toastOptions={{
+          success: {
+            iconTheme: {
+              primary: '#3b9893',
+              secondary: 'white',
+            },
+          },
+        }}
+      />
+      <div className="headings-and-table-container" id="inventory">
+        {/* {isLoading ? (
         <div className="scale-loader-container">
           <ScaleLoader
             color={'#3b9893'}
@@ -242,116 +265,116 @@ export default function Inventory({ tempInStock }) {
           />
         </div>
       ) : ( */}
-      <>
-        <table>
-          <thead>
-            <tr className="tr-inventory-title">
-              <td>
-                <h1>Inventory</h1>
-              </td>
-              <td id="add-prod-td">
-                <DropDownIcon handleDisplayRow={handleDisplayRow} />
-              </td>
-            </tr>
-            <tr className="tr-header">
-              {tableHeader.map((header) => (
-                <td
-                  className={`header-tds 
+        <>
+          <table>
+            <thead>
+              <tr className="tr-inventory-title">
+                <td>
+                  <h1>Inventory</h1>
+                </td>
+                <td id="add-prod-td">
+                  <DropDownIcon handleDisplayRow={handleDisplayRow} />
+                </td>
+              </tr>
+              <tr className="tr-header">
+                {tableHeader.map((header) => (
+                  <td
+                    className={`header-tds 
                   ${header === 'Checkbox' ? 'heading-select' : ''}
                 ${header === 'SKU' ? 'heading-sku' : ''}
                 ${header === 'Name' ? 'heading-name' : ''}
                 ${header === 'Description' ? 'heading-description' : ''}
                 ${header === 'In Stock' ? 'heading-in-stock' : ''}`}
-                  key={header}
-                >
-                  {renderHeaderContent(header)}
-                </td>
-              ))}
-            </tr>
-          </thead>
-          <DragDropContext onDragEnd={handleDragEnd}>
-            <Droppable droppableId="inventory">
-              {(provided, snapshot) => (
-                <tbody
-                  ref={provided.innerRef}
-                  {...provided.droppableProps}
-                  className="inventory-items-container"
-                >
-                  <AddProductRow
-                    rowAdded={rowAdded}
-                    handleHideRow={handleHideRow}
-                    handleHeaderChange={handleHeaderChange}
-                    reloadInventory={handleReloadInventory}
-                  />
-                  {/* this is what creates each list item by mapping over inventory (which is pulled in from context) */}
-                  {inventory.length > 0 ? (
-                    inventory.map((item, index) => (
-                      // use key here to get specific item to get (for popup) update or delete.
-                      // item.sku value - this will scroll to selected value from searchInput.jsx
-                      <Draggable
-                        key={item.id}
-                        draggableId={String(item.id)}
-                        index={index}
-                      >
-                        {(provided, snapshot) => (
-                          <tr
-                            id={item.sku}
-                            ref={provided.innerRef}
-                            {...provided.draggableProps}
-                            {...provided.dragHandleProps}
-                            className={snapshot.isDragging ? 'dragging' : ''}
-                          >
-                            <td className="item-select">
-                              <Checkbox />
-                            </td>
-                            <td id="scrollForAddRow" className="item-sku">
-                              {/* this id catches the scrollintoview when clicking add product */}
-                              {item.sku}
-                            </td>
-                            <td>{item.brand}</td>
-                            <td className="item-name">{item.productName}</td>
-                            <td className="item-description">
-                              <div className="desc-text">
-                                {item.description}
-                              </div>
-                            </td>
-                            <td className="item-in-stock">
-                              {/* {item.inStock} */}
-                              {tempInStock[item.id] || item.inStock}
-                            </td>
-                            <td>
-                              <input
-                                className="dynamic-inputs"
-                                id="reorderAt"
-                                type="text"
-                                defaultValue={item.reorderAt}
-                                onKeyDown={(event) =>
-                                  handleKeyDown(
-                                    event,
-                                    item.id,
-                                    'reorderAt',
-                                    event.target.value
-                                  )
-                                }
-                              />
-                            </td>
-                            <td>
-                              <input
-                                className="dynamic-inputs"
-                                id="orderQty"
-                                type="text"
-                                defaultValue={item.orderQty}
-                                onKeyDown={(event) =>
-                                  handleKeyDown(
-                                    event,
-                                    item.id,
-                                    'orderQty',
-                                    event.target.value
-                                  )
-                                }
-                              />
-                            </td>
-                            {/* <td>
+                    key={header}
+                  >
+                    {renderHeaderContent(header)}
+                  </td>
+                ))}
+              </tr>
+            </thead>
+            <DragDropContext onDragEnd={handleDragEnd}>
+              <Droppable droppableId="inventory">
+                {(provided, snapshot) => (
+                  <tbody
+                    ref={provided.innerRef}
+                    {...provided.droppableProps}
+                    className="inventory-items-container"
+                  >
+                    <AddProductRow
+                      rowAdded={rowAdded}
+                      handleHideRow={handleHideRow}
+                      handleHeaderChange={handleHeaderChange}
+                      reloadInventory={handleReloadInventory}
+                    />
+                    {/* this is what creates each list item by mapping over inventory (which is pulled in from context) */}
+                    {inventory.length > 0 ? (
+                      inventory.map((item, index) => (
+                        // use key here to get specific item to get (for popup) update or delete.
+                        // item.sku value - this will scroll to selected value from searchInput.jsx
+                        <Draggable
+                          key={item.id}
+                          draggableId={String(item.id)}
+                          index={index}
+                        >
+                          {(provided, snapshot) => (
+                            <tr
+                              id={item.sku}
+                              ref={provided.innerRef}
+                              {...provided.draggableProps}
+                              {...provided.dragHandleProps}
+                              className={snapshot.isDragging ? 'dragging' : ''}
+                            >
+                              <td className="item-select">
+                                <Checkbox />
+                              </td>
+                              <td id="scrollForAddRow" className="item-sku">
+                                {/* this id catches the scrollintoview when clicking add product */}
+                                {item.sku}
+                              </td>
+                              <td>{item.brand}</td>
+                              <td className="item-name">{item.productName}</td>
+                              <td className="item-description">
+                                <div className="desc-text">
+                                  {item.description}
+                                </div>
+                              </td>
+                              <td className="item-in-stock">
+                                {/* {item.inStock} */}
+                                {tempInStock[item.id] || item.inStock}
+                              </td>
+                              <td>
+                                <input
+                                  className="dynamic-inputs"
+                                  id="reorderAt"
+                                  type="text"
+                                  defaultValue={item.reorderAt}
+                                  onKeyDown={(event) =>
+                                    handleKeyDown(
+                                      event,
+                                      item.id,
+                                      'reorderAt',
+                                      event.target.value
+                                    )
+                                  }
+                                />
+                              </td>
+                              <td>
+                                <input
+                                  className="dynamic-inputs"
+                                  id="orderQty"
+                                  type="text"
+                                  defaultValue={item.orderQty}
+                                  onKeyDown={(event) =>
+                                    handleKeyDown(
+                                      event,
+                                      item.id,
+                                      'orderQty',
+                                      event.target.value
+                                    )
+                                  }
+                                />
+                              </td>
+                              {/* <td>
                                 <button
                                   id="incoming"
                                   onClick={(event) =>
@@ -365,21 +388,21 @@ export default function Inventory({ tempInStock }) {
                                   />
                                 </button>
                               </td> */}
-                            <td>
-                              <button
-                                id="order"
-                                onClick={(event) => {
-                                  handleOpenPopup(item, event);
-                                }}
-                              >
-                                <FontAwesomeIcon
-                                  icon="fa-bag-shopping"
-                                  className="fa-icon"
-                                  style={{ pointerEvents: 'none' }}
-                                />
-                              </button>
-                            </td>
-                            {/* <td>
+                              <td>
+                                <button
+                                  id="order"
+                                  onClick={(event) => {
+                                    handleOpenPopup(item, event);
+                                  }}
+                                >
+                                  <FontAwesomeIcon
+                                    icon="fa-bag-shopping"
+                                    className="fa-icon"
+                                    style={{ pointerEvents: 'none' }}
+                                  />
+                                </button>
+                              </td>
+                              {/* <td>
                                 <button
                                   id="settings"
                                   onClick={(event) =>
@@ -393,45 +416,46 @@ export default function Inventory({ tempInStock }) {
                                   />
                                 </button>
                               </td> */}
-                          </tr>
-                        )}
-                      </Draggable>
-                    ))
-                  ) : (
-                    <tr>
-                      <td colSpan={10}>No inventory data available.</td>
-                    </tr>
-                  )}
-                  {provided.placeholder}
-                </tbody>
-              )}
-            </Droppable>
-          </DragDropContext>
-        </table>
-      </>
-      {popup == 'incoming' && (
-        <IncomingPopup
-          handleClosePopup={handleClosePopup}
-          popup={popup}
-          item={productForPopup}
-          reloadInventory={handleReloadInventory}
-        />
-      )}
-      {popup == 'order' && (
-        <OrderNowPopup
-          handleClosePopup={handleClosePopup}
-          popup={popup}
-          item={productForPopup}
-          reloadInventory={handleReloadInventory}
-        />
-      )}
-      {popup == 'selectedCheckboxOptions' && (
-        <SelectedCheckboxOptionsPopup
-          handleClosePopup={handleClosePopup}
-          popup={popup}
-          reloadInventory={handleReloadInventory}
-        />
-      )}
-    </div>
+                            </tr>
+                          )}
+                        </Draggable>
+                      ))
+                    ) : (
+                      <tr>
+                        <td colSpan={10}>No inventory data available.</td>
+                      </tr>
+                    )}
+                    {provided.placeholder}
+                  </tbody>
+                )}
+              </Droppable>
+            </DragDropContext>
+          </table>
+        </>
+        {popup == 'incoming' && (
+          <IncomingPopup
+            handleClosePopup={handleClosePopup}
+            popup={popup}
+            item={productForPopup}
+            reloadInventory={handleReloadInventory}
+          />
+        )}
+        {popup == 'order' && (
+          <OrderNowPopup
+            handleClosePopup={handleClosePopup}
+            popup={popup}
+            item={productForPopup}
+            reloadInventory={handleReloadInventory}
+          />
+        )}
+        {popup == 'selectedCheckboxOptions' && (
+          <SelectedCheckboxOptionsPopup
+            handleClosePopup={handleClosePopup}
+            popup={popup}
+            reloadInventory={handleReloadInventory}
+          />
+        )}
+      </div>
+    </>
   );
 }
