0e42aee6c93e93e63d0a52ab152691d4b6150df7
Breland <claybreland1@gmail.com>
2023-03-23 12:36:48 -0500

    delete multiple products is working from checkbox options popup

--- client/src/components/Inventory/popups/CheckboxOptions.jsx
+++ client/src/components/Inventory/popups/CheckboxOptions.jsx
@@ -24,90 +24,114 @@ export default function SelectedCheckboxOptions({ handleClosePopup, popup }) {
         return inventory.find(product => product.id === id);
     }
 
+    const [message, setMessage] = useState(null);
     async function handleDeleteProductsAndOrders(event) {
-        await deleteInventoryItems(selectedItems)
-        // await deleteOrders(selectedItems)
-        handleClosePopup(event)
+        try {
+            await deleteInventoryItems(selectedItems);
+            // await deleteOrders(selectedItems);
+            setMessage("Products deleted successfully.");
+            setTimeout(() => {
+                setMessage(null);
+            }, 3000); // 3 seconds timeou
+            reloadInventory()
+        } catch (error) {
+            setMessage("An error occurred while deleting products.");
+            console.error("Error deleting products:", error);
+        } finally {
+            handleClosePopup(event);
+        }
     }
 
+
     return (
-        <div className="popup checkbox-options-popup">
-            <h2>Checkbox Selection Options</h2>
-            <section className='options-section'>
-                <div className='rows'>
-                    <div className=''>Select All</div>
-                    <Switch />
-                </div>
-                <div className='rows'>
-                    <div className=''>Highlight Selected</div>
-                    <Switch />
-                </div>
-                <div className='rows '>
-                    <div className=''>Display list to delete all or create orders for selected</div>
-                    <Switch
-                        className="display-list"
-                        checked={displayConfirmation}
-                        onChange={handleDisplayConfirmationToggle}
-                    />
-                </div>
-
-            </section>
-            {/* this section is displayed when the toggle switch for "display list..." is on */}
-            {displayConfirmation && (
-                <section className='confirmation'>
-                    <h4>Confirm</h4>
-                    <div className='listOfSelectedContainer'>
-                        <ul>
-                            {
-                                selectedItems.map((id, index) => {
-                                    console.log(id)
-                                    const product = findProductById(id);
-                                    return (
-                                        <li key={id}>
-                                            <div className='list-numbering'>
-                                                {index + 1}
-                                            </div>
-                                            <div>
-                                                {product.sku}
-                                            </div>
-                                            <div className='list-spacer'>
-                                                -
-                                            </div>
-                                            <div>
-                                                {product.productName}
-                                            </div>
-                                        </li>
-                                    );
-                                })}
-
-                        </ul>
+        <>
+            <div className="popup checkbox-options-popup">
+                <h2>Checkbox Selection Options</h2>
+                <section className='options-section'>
+                    <div className='rows'>
+                        <div className=''>Select All</div>
+                        <Switch />
                     </div>
                     <div className='rows'>
-                        <p>Delete {selectedItems.length} selected products and any orders associated with them</p>
-                        <button>
-                            <FontAwesomeIcon
-                                icon={faTrash}
-                                onClick={handleDeleteProductsAndOrders}
-                            >
-                            </FontAwesomeIcon>
-                        </button>
+                        <div className=''>Highlight Selected</div>
+                        <Switch />
                     </div>
-                    <div className='rows'>
-                        <p>Create a one time order for each of the {selectedItems.length} selected products</p>
-                        <button>
-                            <FontAwesomeIcon
-                                icon={faShoppingBag}>
-                            </FontAwesomeIcon>
-                        </button>
+                    <div className='rows '>
+                        <div className=''>Display list to delete all or create orders for selected</div>
+                        <Switch
+                            className="display-list"
+                            checked={displayConfirmation}
+                            onChange={handleDisplayConfirmationToggle}
+                        />
                     </div>
-
 
                 </section>
-            )}
-            <section>
-                <button id="close" onClick={(event) => handleClosePopup(event)} className={popup === "close" ? "hide" : "show"}>Close</button>
-            </section>
-        </div>
+                {/* this section is displayed when the toggle switch for "display list..." is on */}
+                {displayConfirmation && (
+                    <section className='confirmation'>
+                        <h4>Confirm</h4>
+                        <div className='listOfSelectedContainer'>
+                            <ul>
+                                {
+                                    selectedItems.map((id, index) => {
+                                        console.log(id)
+                                        const product = findProductById(id);
+                                        return (
+                                            <li key={id}>
+                                                <div className='list-numbering'>
+                                                    {index + 1}
+                                                </div>
+                                                <div>
+                                                    {product.sku}
+                                                </div>
+                                                <div className='list-spacer'>
+                                                    -
+                                                </div>
+                                                <div>
+                                                    {product.productName}
+                                                </div>
+                                            </li>
+                                        );
+                                    })}
 
+                            </ul>
+                        </div>
+                        <div className='rows'>
+                            <p>Delete {selectedItems.length} selected products and any orders associated with them</p>
+                            <button
+                                onClick={handleDeleteProductsAndOrders}
+                            >
+                                <FontAwesomeIcon
+                                    icon={faTrash}
+                                >
+                                </FontAwesomeIcon>
+                            </button>
+                        </div>
+                        <div className='rows'>
+                            <p>Create a one time order for each of the {selectedItems.length} selected products</p>
+                            <button
+
+                            >
+                                <FontAwesomeIcon
+                                    icon={faShoppingBag}>
+                                </FontAwesomeIcon>
+                            </button>
+                        </div>
+
+
+                    </section>
+                )}
+                <section>
+                    <button id="close" onClick={(event) => handleClosePopup(event)} className={popup === "close" ? "hide" : "show"}>Close</button>
+                </section>
+            </div>
+            {
+                message && (
+                    <div className="popup-message popup">
+                        <p>{message}</p>
+                    </div>
+                )
+            }
+        </>
     )
 }
