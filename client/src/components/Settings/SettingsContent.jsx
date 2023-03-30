62f6e73da186401c720f697f4357f8e5f3ead092
Joshua Ow <joshuaow@gmail.com>
2023-03-29 22:09:29 -0500

    added pin and unpin on settings page to test out function. without it saving the pin to DB we may be better off with drag and drop

--- client/src/components/Settings/SettingsContent.jsx
+++ client/src/components/Settings/SettingsContent.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState} from 'react';
 import { useNavigate } from 'react-router-dom';
 import { authenticateUser } from '../../services/authenticationAPIcalls';
 import { useQuery } from 'react-query';
@@ -27,11 +27,61 @@ function SettingsContent() {
       confirmButtonColor: '#3b9893',
     });
   }
+
+  const dataTwo = [
+  { id: 1, name: 'Item 1' },
+  { id: 2, name: 'Item 2' },
+  { id: 3, name: 'Item 3' },
+  { id: 4, name: 'Item 4' },
+];
+
+const TableRow = ({ item, onPin, onUnpin, isPinned }) => (
+  <tr>
+    <td>{item.name}</td>
+    <td>
+      <button onClick={() => (isPinned ? onUnpin(item) : onPin(item))}>
+        {isPinned ? 'Unpin' : 'Pin'}
+      </button>
+    </td>
+  </tr>
+);
+
+  const [pinnedItems, setPinnedItems] = useState([]);
+  const [unpinnedItems, setUnpinnedItems] = useState(dataTwo);
+
+  const handlePin = (item) => {
+    setPinnedItems([...pinnedItems, item]);
+    setUnpinnedItems(unpinnedItems.filter((i) => i.id !== item.id));
+  };
+
+  const handleUnpin = (item) => {
+    setUnpinnedItems([...unpinnedItems, item]);
+    setPinnedItems(pinnedItems.filter((i) => i.id !== item.id));
+  };
+
   return (
     <div>
-      <h1>Content TBD</h1>
+      <h1>Pinned Rows</h1>
+      <table>
+        <tbody>
+          {pinnedItems.map((item) => (
+            <TableRow key={item.id} item={item} onUnpin={handleUnpin} isPinned />
+          ))}
+        </tbody>
+      </table>
+
+      <h1>Unpinned Rows</h1>
+      <table>
+        <tbody>
+          {unpinnedItems.map((item) => (
+            <TableRow key={item.id} item={item} onPin={handlePin} />
+          ))}
+        </tbody>
+      </table>
     </div>
   );
-}
+};
+
+
 
 export default SettingsContent;
