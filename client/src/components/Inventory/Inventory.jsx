3d89c48a07f2d1270b2a1d32392f8714732e766a
Joshua Ow <joshuaow@gmail.com>
2023-08-21 14:27:06 -0500

    rows can now be toggled, the button at the top will open a pop up with info for these rows. now the logic from the previous checkbox component needs to be implemented.

--- client/src/components/Inventory/Inventory.jsx
+++ client/src/components/Inventory/Inventory.jsx
@@ -16,10 +16,10 @@ import {
 import Order from "./modals/OrderNow";
 import { OrdersContext } from "../../contexts/orders.context";
 import calculateTotal from "../../utils/calcShippingAndTotal";
-import { CustomCheckbox } from "./CustomCheckbox";
 import AddProductButton from "./modals/AddProductButton";
 import OrderHistory from "../Orders/OrderHistory";
 import ActiveOrders from "../Orders/ActiveOrders";
+import SelectedRowsModal from "./modals/SelectedRows";
 
 export default function Inventory() {
   const {
@@ -42,6 +42,7 @@ export default function Inventory() {
 
   const [popup, setPopup] = useState(null);
   const [activeTab, setActiveTab] = useState("inventory");
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const handleShowPopup = (product) => {
     setProductForPopup(product);
@@ -66,6 +67,10 @@ export default function Inventory() {
     setProductForPopup(null);
   };
 
+  const openModalWithSelectedRows = () => {
+    setIsModalOpen(true);
+  };
+
   // -------------------- Trigger orders at reorder at points ------------------------------
 
   const handleCalculateTotals = (orderQty, unitPrice) => {
@@ -83,6 +88,23 @@ export default function Inventory() {
   const handleReloadInventory = () => {
     reloadInventory();
   };
+
+  const IndeterminateCheckbox = React.forwardRef(
+    ({ indeterminate, ...rest }, ref) => {
+      const defaultRef = React.useRef();
+      const resolvedRef = ref || defaultRef;
+
+      React.useEffect(() => {
+        resolvedRef.current.indeterminate = indeterminate;
+      }, [resolvedRef, indeterminate]);
+
+      return (
+        <>
+          <input type="checkbox" ref={resolvedRef} {...rest} />
+        </>
+      );
+    }
+  );
 
   // keeping this old inventoryUsage code for now as backup
   useEffect(() => {
@@ -131,7 +153,7 @@ export default function Inventory() {
   );
 
   const columns = useMemo(
-    () => [   
+    () => [
       {
         Header: "SKU",
         accessor: "sku",
@@ -179,25 +201,6 @@ export default function Inventory() {
           </button>
         ),
       },
-      {
-        Header: "Options",
-        accessor: "checkbox",
-        Cell: ({ row }) => (
-          <CustomCheckbox
-            itemId={row.original.id}
-            onChange={toggleSelectedItem}
-            selectedItems={selectedItems}
-            handleOpenPopup={handleOpenPopup} 
-            sx={{
-              "&.Mui-checked": {
-                "& .MuiSvgIcon-root": {
-                  fill: "var(--accent-color)",
-                },
-              },
-            }}
-          />
-        ),
-      },
     ],
     []
   );
@@ -221,15 +224,21 @@ export default function Inventory() {
     useSortBy,
     usePagination,
     useRowSelect,
-    hooks => {
-      hooks.visibleColumns.push(columns => [
+    (hooks) => {
+      hooks.visibleColumns.push((columns) => [
         {
-          id: 'selection',
+          id: "selection",
           Header: ({ getToggleAllRowsSelectedProps }) => (
-            <CustomCheckbox {...getToggleAllRowsSelectedProps()} />
+            <div>
+              <IndeterminateCheckbox
+                {...getToggleAllRowsSelectedProps()}
+                onClick={openModalWithSelectedRows}
+              />
+              <button onClick={openModalWithSelectedRows}>Show Selected</button>
+            </div>
           ),
           Cell: ({ row }) => (
-            <CustomCheckbox {...row.getToggleRowSelectedProps()} />
+            <IndeterminateCheckbox {...row.getToggleRowSelectedProps()} />
           ),
         },
         ...columns,
@@ -242,159 +251,168 @@ export default function Inventory() {
       {isLoading ? (
         <div>Loading...</div>
       ) : (
-        <div className="bg-zinc-100 rounded-2xl p-4 overflow-x-auto">
-          <div className="flex mb-4 gap-x-1 font-semibold text-zinc-800 px-2">
-            <button
-              className={`px-4 py-2 ${
-                activeTab === "inventory" ? "bg-emerald-400/75 rounded-xl text-emerald-800" : "text-zinc-700"
-              }`}
-              onClick={() => setActiveTab("inventory")}
-            >
-              <FontAwesomeIcon icon={faBox} className="mr-1 text-zinc-600" />{" "}
-              Inventory
-            </button>
-            <button
-              className={`px-4 py-2 ${
-                activeTab === "Active Orders"
-                  ? "bg-emerald-400/75 rounded-xl text-emerald-800"
-                  : "text-zinc-700"
-              }`}
-              onClick={() => setActiveTab("Active Orders")}
-            >
-              <FontAwesomeIcon
-                icon={faShoppingCart}
-                className="mr-1 text-zinc-600"
-              />{" "}
-              Active Orders
-            </button>
-            <button
-              className={`px-4 py-2 ${
-                activeTab === "Order History"
-                  ? "bg-emerald-400/75 rounded-xl text-emerald-800"
-                  : "text-zinc-700"
-              }`}
-              onClick={() => setActiveTab("Order History")}
-            >
-              <FontAwesomeIcon
-                icon={faArchive}
-                className="mr-1 text-zinc-600"
-              />{" "}
-              Order History
-            </button>
-          </div>
-
-          {activeTab === "inventory" && (
-            <>
-              <div className="flex justify-end mx-4 mb-3">
-                <AddProductButton />
-              </div>
-              <table
-                {...getTableProps()}
-                id="inventory"
-                className="w-full table-auto text-black/80"
+        <>
+          <SelectedRowsModal
+            isOpen={isModalOpen}
+            onClose={() => setIsModalOpen(false)}
+            selectedRows={selectedFlatRows.map((r) => r.original)}
+          />
+          <div className="bg-zinc-100 rounded-2xl p-4 overflow-x-auto">
+            <div className="flex mb-4 gap-x-1 font-semibold text-zinc-800 px-2">
+              <button
+                className={`px-4 py-2 ${
+                  activeTab === "inventory"
+                    ? "bg-emerald-400/75 rounded-xl text-emerald-800"
+                    : "text-zinc-700"
+                }`}
+                onClick={() => setActiveTab("inventory")}
               >
-                <thead className="border-b border-zinc-200 h-14 text-sm ">
-                  {headerGroups.map((headerGroup) => (
-                    <tr {...headerGroup.getHeaderGroupProps()}>
-                      {headerGroup.headers.map((column) => (
-                        <th
-                          {...column.getHeaderProps(
-                            column.getSortByToggleProps()
-                          )}
-                          className="px-1 font-semibold"
-                        >
-                          {column.render("Header")}
-                          <span className="">
-                            {column.isSorted ? (
-                              column.isSortedDesc ? (
-                                <FontAwesomeIcon
-                                  icon={faSortDown}
-                                  className="text-zinc-400 ml-2"
-                                />
+                <FontAwesomeIcon icon={faBox} className="mr-1 text-zinc-600" />{" "}
+                Inventory
+              </button>
+              <button
+                className={`px-4 py-2 ${
+                  activeTab === "Active Orders"
+                    ? "bg-emerald-400/75 rounded-xl text-emerald-800"
+                    : "text-zinc-700"
+                }`}
+                onClick={() => setActiveTab("Active Orders")}
+              >
+                <FontAwesomeIcon
+                  icon={faShoppingCart}
+                  className="mr-1 text-zinc-600"
+                />{" "}
+                Active Orders
+              </button>
+              <button
+                className={`px-4 py-2 ${
+                  activeTab === "Order History"
+                    ? "bg-emerald-400/75 rounded-xl text-emerald-800"
+                    : "text-zinc-700"
+                }`}
+                onClick={() => setActiveTab("Order History")}
+              >
+                <FontAwesomeIcon
+                  icon={faArchive}
+                  className="mr-1 text-zinc-600"
+                />{" "}
+                Order History
+              </button>
+            </div>
+
+            {activeTab === "inventory" && (
+              <>
+                <div className="flex justify-end mx-4 mb-3">
+                  <AddProductButton />
+                </div>
+                <table
+                  {...getTableProps()}
+                  id="inventory"
+                  className="w-full table-auto text-black/80"
+                >
+                  <thead className="border-b border-zinc-200 h-14 text-sm ">
+                    {headerGroups.map((headerGroup) => (
+                      <tr {...headerGroup.getHeaderGroupProps()}>
+                        {headerGroup.headers.map((column) => (
+                          <th
+                            {...column.getHeaderProps(
+                              column.getSortByToggleProps()
+                            )}
+                            className="px-1 font-semibold"
+                          >
+                            {column.render("Header")}
+                            <span className="">
+                              {column.isSorted ? (
+                                column.isSortedDesc ? (
+                                  <FontAwesomeIcon
+                                    icon={faSortDown}
+                                    className="text-zinc-400 ml-2"
+                                  />
+                                ) : (
+                                  <FontAwesomeIcon
+                                    icon={faSortUp}
+                                    className="text-zinc-400 ml-2"
+                                  />
+                                )
                               ) : (
                                 <FontAwesomeIcon
-                                  icon={faSortUp}
+                                  icon={faSort}
                                   className="text-zinc-400 ml-2"
                                 />
-                              )
-                            ) : (
-                              <FontAwesomeIcon
-                                icon={faSort}
-                                className="text-zinc-400 ml-2"
-                              />
-                            )}
-                          </span>
-                        </th>
-                      ))}
-                    </tr>
-                  ))}
-                </thead>
-                <tbody {...getTableBodyProps()} className="tracking-wide">
-                  {page.map((row) => {
-                    prepareRow(row);
-                    return (
-                      <tr
-                        {...row.getRowProps()}
-                        className="text-sm h-12 border-b last:border-none border-zinc-200 hover:bg-zinc-50"
-                      >
-                        {row.cells.map((cell) => (
-                          <td {...cell.getCellProps()} className="px-7">
-                            {cell.render("Cell")}
-                          </td>
+                              )}
+                            </span>
+                          </th>
                         ))}
                       </tr>
-                    );
-                  })}
-                </tbody>
-              </table>
-              <div className="flex gap-4 justify-between p-2 mt-6">
-                <div className="flex gap-4 items-center">
-                  <button
-                    onClick={() => previousPage()}
-                    disabled={!canPreviousPage}
-                  >
-                    {
-                      <FontAwesomeIcon
-                        icon={faCircleChevronLeft}
-                        className="text-xl text-zinc-400 hover:text-zinc-400/80"
-                      />
-                    }
-                  </button>{" "}
-                  <span className="text-sm text-zinc-700">
-                    Page{" "}
-                    <strong>
-                      {pageIndex + 1} of {pageOptions.length}
-                    </strong>{" "}
-                  </span>
-                  <button onClick={() => nextPage()} disabled={!canNextPage}>
-                    {
-                      <FontAwesomeIcon
-                        icon={faCircleChevronRight}
-                        className="text-xl text-zinc-400 hover:text-zinc-400/80"
-                      />
-                    }
-                  </button>{" "}
-                </div>
+                    ))}
+                  </thead>
+                  <tbody {...getTableBodyProps()} className="tracking-wide">
+                    {page.map((row) => {
+                      prepareRow(row);
+                      return (
+                        <tr
+                          {...row.getRowProps()}
+                          className="text-sm h-12 border-b last:border-none border-zinc-200 hover:bg-zinc-50"
+                        >
+                          {row.cells.map((cell) => (
+                            <td {...cell.getCellProps()} className="px-7">
+                              {cell.render("Cell")}
+                            </td>
+                          ))}
+                        </tr>
+                      );
+                    })}
+                  </tbody>
+                </table>
+                <div className="flex gap-4 justify-between p-2 mt-6">
+                  <div className="flex gap-4 items-center">
+                    <button
+                      onClick={() => previousPage()}
+                      disabled={!canPreviousPage}
+                    >
+                      {
+                        <FontAwesomeIcon
+                          icon={faCircleChevronLeft}
+                          className="text-xl text-zinc-400 hover:text-zinc-400/80"
+                        />
+                      }
+                    </button>{" "}
+                    <span className="text-sm text-zinc-700">
+                      Page{" "}
+                      <strong>
+                        {pageIndex + 1} of {pageOptions.length}
+                      </strong>{" "}
+                    </span>
+                    <button onClick={() => nextPage()} disabled={!canNextPage}>
+                      {
+                        <FontAwesomeIcon
+                          icon={faCircleChevronRight}
+                          className="text-xl text-zinc-400 hover:text-zinc-400/80"
+                        />
+                      }
+                    </button>{" "}
+                  </div>
 
-                <select
-                  value={pageSize}
-                  onChange={(e) => {
-                    setPageSize(Number(e.target.value));
-                  }}
-                  className="rounded-xl bg-zinc-300 text-zinc-800 text-sm outline-none p-1"
-                >
-                  {[10, 20, 30, 50].map((pageSize) => (
-                    <option key={pageSize} value={pageSize}>
-                      Show {pageSize}
-                    </option>
-                  ))}
-                </select>
-              </div>
-            </>
-          )}
-          {activeTab === "Active Orders" && <ActiveOrders />}
-          {activeTab === "Order History" && <OrderHistory />}
-        </div>
+                  <select
+                    value={pageSize}
+                    onChange={(e) => {
+                      setPageSize(Number(e.target.value));
+                    }}
+                    className="rounded-xl bg-zinc-300 text-zinc-800 text-sm outline-none p-1"
+                  >
+                    {[10, 20, 30, 50].map((pageSize) => (
+                      <option key={pageSize} value={pageSize}>
+                        Show {pageSize}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+              </>
+            )}
+            {activeTab === "Active Orders" && <ActiveOrders />}
+            {activeTab === "Order History" && <OrderHistory />}
+          </div>
+        </>
       )}
 
       {popup === "order" && (
