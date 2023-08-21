4cd829a931242c4f4456dcc9cbd03bcff758ddab
Joshua Ow <joshuaow@gmail.com>
2023-08-21 10:32:45 -0500

    sparklines for modules, need to get more specific about the data, add things to schema

--- client/src/components/Inventory/Stats.jsx
+++ client/src/components/Inventory/Stats.jsx
@@ -1,23 +1,23 @@
 import React, { useState, useEffect, useContext } from "react";
+import ReactApexChart from "react-apexcharts";
 import { API_URL } from "../../services/config";
 import axios from "axios";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { AuthContext } from "../../contexts/auth.context";
 import { InventoryContext } from "../../contexts/inventory.context";
-
-import {
-  faBoxOpen,
-  faChartLine,
-  faShippingFast,
-} from "@fortawesome/free-solid-svg-icons";
+import { faBoxOpen, faChartLine, faShippingFast } from "@fortawesome/free-solid-svg-icons";
 
 function Stats() {
   const { userId } = useContext(AuthContext);
   const { needStatsUpdate, setNeedStatsUpdate } = useContext(InventoryContext);
+
   const [data, setData] = useState({
     totalInventoryItems: 0,
     totalActiveOrders: 0,
     totalSales: 0,
+    inventoryItemsSpark: [],
+    activeOrdersSpark: [],
+    salesSpark: [],
   });
 
   useEffect(() => {
@@ -34,36 +34,66 @@ function Stats() {
     }
   }, [userId, needStatsUpdate]);
 
+  const sparkOptions = {
+    chart: {
+      id: "basic-bar",
+      type: "line",
+      height: 50,
+      sparkline: {
+        enabled: true
+      },
+    },
+    stroke: {
+      curve: 'smooth',
+    },
+    fill: {
+      opacity: 1
+    },
+    colors: ['rgb(63, 191, 128)']
+  };
+  
+
   return (
     <div className="flex text-zinc-800 gap-6 ">
-      <div className="bg-zinc-100 rounded-2xl p-4 w-52 flex flex-col gap-2">
+      <div className="bg-zinc-100 rounded-2xl p-4 w-1/3 flex flex-col gap-2">
         <h3>
-          {" "}
           <FontAwesomeIcon
             icon={faBoxOpen}
             className="mr-1 text-emerald-400"
-          />{" "}
+          />
           Inventory Items
         </h3>
         <p className="text-3xl font-semibold text-zinc-600">
           {data.totalInventoryItems}
         </p>
+        <ReactApexChart
+          options={sparkOptions}
+          series={[{ data: data.inventoryItemsSpark }]}
+          type="area"
+        />
       </div>
-      <div className="bg-zinc-100 rounded-2xl p-4 w-52 flex flex-col gap-2">
+      
+      <div className="bg-zinc-100 rounded-2xl p-4 w-1/3 flex flex-col gap-2">
         <h3>
           <FontAwesomeIcon
             icon={faShippingFast}
             className="mr-1 text-emerald-400"
-          />{" "}
+          />
           Total Active Orders:
         </h3>
         <p className="text-3xl font-semibold text-zinc-600">
           {data.totalActiveOrders}
         </p>
+        <ReactApexChart
+          options={sparkOptions}
+          series={[{ data: data.activeOrdersSpark }]}
+          type="area"
+        />
       </div>
-      <div className="bg-zinc-100 rounded-2xl p-4 w-52 flex flex-col gap-2">
+      
+      <div className="bg-zinc-100 rounded-2xl p-4 w-1/3 flex flex-col gap-2">
         <h3>
-          <FontAwesomeIcon icon={faChartLine} className="mr-1 text-emerald-400" />{" "}
+          <FontAwesomeIcon icon={faChartLine} className="mr-1 text-emerald-400" />
           Total Sales:
         </h3>
         <p className="text-3xl font-semibold text-zinc-600">
@@ -72,6 +102,11 @@ function Stats() {
             currency: "USD",
           })}
         </p>
+        <ReactApexChart
+          options={sparkOptions}
+          series={[{ data: data.salesSpark }]}
+          type="area"
+        />
       </div>
     </div>
   );
