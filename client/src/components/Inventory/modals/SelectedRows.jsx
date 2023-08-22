b78e67ebd4f173a91545e85c6e599b19eb099200
Joshua Ow <joshuaow@gmail.com>
2023-08-22 09:23:43 -0500

    styled the options modal. replaced window confirm with a confirm modal

--- client/src/components/Inventory/modals/SelectedRows.jsx
+++ client/src/components/Inventory/modals/SelectedRows.jsx
@@ -1,6 +1,9 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { InventoryContext } from "../../../contexts/inventory.context";
 import { deleteInventoryItems } from "../../../services/inventoryAPIcalls";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes, faTrash } from "@fortawesome/free-solid-svg-icons";
+import ConfirmModal from "./ConfirmModal";
 
 export default function SelectedRowsModal({ isOpen, onClose, selectedRows }) {
   const {
@@ -10,25 +13,27 @@ export default function SelectedRowsModal({ isOpen, onClose, selectedRows }) {
     selectedItems,
     setSelectedItems,
     toggleSelectedItem,
+    setNeedStatsUpdate,
   } = useContext(InventoryContext);
 
-  async function handleDeleteProducts() {
-    console.log("Selected rows:", selectedRows);
+  const [confirmModalOpen, setConfirmModalOpen] = useState(false);
 
-    const result = window.confirm(
-      "Are you sure you want to delete the selected items?"
-    );
-
-    if (result) {
-      try {
-        const itemIdsToDelete = selectedRows.map((row) => row.id);
-        await deleteInventoryItems(itemIdsToDelete);
-        reloadInventory();
-      } catch (error) {
-        console.error("Error deleting products:", error);
-        throw new Error("An error occurred while deleting products.");
-      }
+  async function confirmDelete() {
+    try {
+      const itemIdsToDelete = selectedRows.map((row) => row.id);
+      await deleteInventoryItems(itemIdsToDelete);
+      reloadInventory();
+      setNeedStatsUpdate(true);
+      setConfirmModalOpen(false);
+      onClose();
+    } catch (error) {
+      console.error("Error deleting products:", error);
+      throw new Error("An error occurred while deleting products.");
     }
+  }
+
+  function handleDeleteProducts() {
+    setConfirmModalOpen(true);
   }
 
   if (!isOpen) {
@@ -38,47 +43,59 @@ export default function SelectedRowsModal({ isOpen, onClose, selectedRows }) {
   return (
     <div className="fixed z-10 inset-0 overflow-y-auto">
       <div className="flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center">
-        <div className="fixed inset-0 transition-opacity" aria-hidden="true">
-          <div className="absolute inset-0 bg-black/50"></div>
-        </div>
+        <div
+          className="fixed inset-0 bg-black/50 transition-opacity"
+          aria-hidden="true"
+        ></div>
 
         <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
-          <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-            <div className="sm:flex sm:items-start">
-              <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                <h3 className="text-lg leading-6 font-medium text-gray-900">
-                  Options
-                </h3>
-                <div className="mt-2">
-                  <ul>
-                    {selectedRows.map((row) => (
-                      <li key={row.id}>
-                        {row.id} - {row.sku} - {row.productName}
-                      </li>
-                    ))}
-                  </ul>
-                  <button
-                    type="button"
-                    className="bg-red-600 hover:bg-red-700 ..."
-                    onClick={() => handleDeleteProducts(selectedItems)}
-                  >
-                    Delete
-                  </button>
-                </div>
-              </div>
-            </div>
+          <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4 flex justify-between items-start">
+            <h3 className="text-xl  font-bold text-zinc-700">Options</h3>
+            <FontAwesomeIcon
+              icon={faTimes}
+              className="cursor-pointer text-xl text-zinc-500 hover:text-zinc-400"
+              onClick={onClose}
+            />
           </div>
-          <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+          <div className="mt-2 px-4 py-4 sm:p-6">
+            <table className="min-w-full">
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>SKU</th>
+                  <th>Name</th>
+                </tr>
+              </thead>
+              <tbody>
+                {selectedRows.map((row) => (
+                  <tr key={row.id}>
+                    <td>{row.id}</td>
+                    <td>{row.sku}</td>
+                    <td>{row.productName}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
             <button
               type="button"
-              className="bg-emerald-400/80 hover:bg-emerald-400 w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 text-base font-medium text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-emerald-500 sm:ml-3 sm:w-auto sm:text-sm"
-              onClick={onClose}
+              className="mt-4 bg-rose-500 font-bold hover:bg-rose-500/80 text-rose-100 p-2 px-3 rounded-lg"
+              onClick={() => handleDeleteProducts(selectedItems)}
             >
-              Close
+              <FontAwesomeIcon icon={faTrash} className="mr-1 text-rose-700" />{" "}
+              Delete
             </button>
           </div>
         </div>
       </div>
+      <ConfirmModal
+        isOpen={confirmModalOpen}
+        onClose={() => {
+          setConfirmModalOpen(false);
+          onClose();
+        }}
+        onConfirm={confirmDelete}
+        message="Are you sure you want to delete the selected items?"
+      />
     </div>
   );
 }
