7b43a0e6b375ad69daa8f571f2a5cc642eeaf707
Joshua Ow <joshuaow@gmail.com>
2023-08-23 09:23:45 -0500

    charts are now synched together to toggle open or closed

--- client/src/components/Inventory/Stats.jsx
+++ client/src/components/Inventory/Stats.jsx
@@ -9,10 +9,17 @@ import {
   faBoxOpen,
   faChartLine,
   faShippingFast,
+  faChevronUp,
+  faChevronDown,
 } from "@fortawesome/free-solid-svg-icons";
 
 function Stats() {
   const { userId } = useContext(AuthContext);
+  const [isOpen, setIsOpen] = useState({
+    inventory: false,
+    orders: false,
+    sales: false,
+  });
   const { needStatsUpdate, setNeedStatsUpdate } = useContext(InventoryContext);
 
   const [data, setData] = useState({
@@ -55,63 +62,111 @@ function Stats() {
     colors: ["rgb(63, 191, 128)"],
   };
 
+  const toggleOpen = () => {
+    setIsOpen((prev) => ({
+      inventory: !prev.inventory,
+      orders: !prev.orders,
+      sales: !prev.sales,
+    }));
+  };
+
   return (
     <div className="flex text-zinc-800 gap-6">
       <div className="bg-zinc-100 rounded-2xl p-4 w-1/3 flex flex-col gap-2">
-        <h3 className="font-bold">
-          <FontAwesomeIcon icon={faBoxOpen} className="mr-2 text-zinc-600" />
-          Inventory Items
-        </h3>
-        <p className="text-3xl font-semibold text-zinc-600">
-          {data.totalInventoryItems}
-        </p>
-        <ReactApexChart
-          options={sparkOptions}
-          series={[{ data: data.inventoryItemsSpark }]}
-          type="area"
-          height={'80px'}
-        />
+        <div
+          className="flex justify-between cursor-pointer"
+          onClick={toggleOpen}
+        >
+          <h3 className="font-bold">
+            <FontAwesomeIcon icon={faBoxOpen} className="mr-2 text-zinc-600" />
+            Inventory Items
+          </h3>
+          <FontAwesomeIcon
+            icon={isOpen.inventory ? faChevronUp : faChevronDown}
+            className="text-zinc-400"
+          />
+        </div>
+
+        {isOpen.inventory && (
+          <>
+            <p className="text-3xl font-semibold text-zinc-600">
+              {data.totalInventoryItems}
+            </p>
+            <ReactApexChart
+              options={sparkOptions}
+              series={[{ data: data.inventoryItemsSpark }]}
+              type="area"
+              height={"80px"}
+            />
+          </>
+        )}
       </div>
 
       <div className="bg-zinc-100 rounded-2xl p-4 w-1/3 flex flex-col gap-2">
-        <h3 className="font-bold">
+        <div
+          className="flex justify-between cursor-pointer"
+          onClick={toggleOpen}
+        >
+          <h3 className="font-bold">
+            <FontAwesomeIcon
+              icon={faShippingFast}
+              className="mr-2 text-zinc-600"
+            />
+            Total Active Orders:
+          </h3>
           <FontAwesomeIcon
-            icon={faShippingFast}
-            className="mr-2 text-zinc-600"
+            icon={isOpen.inventory ? faChevronUp : faChevronDown}
+            className="text-zinc-400"
           />
-          Total Active Orders:
-        </h3>
-        <p className="text-3xl font-semibold text-zinc-600">
-          {data.totalActiveOrders}
-        </p>
-        <ReactApexChart
-          options={sparkOptions}
-          series={[{ data: data.activeOrdersSpark }]}
-          type="area"
-          height={'80px'}
-        />
+        </div>
+        {isOpen.orders && (
+          <>
+            <p className="text-3xl font-semibold text-zinc-600">
+              {data.totalActiveOrders}
+            </p>
+            <ReactApexChart
+              options={sparkOptions}
+              series={[{ data: data.activeOrdersSpark }]}
+              type="area"
+              height={"80px"}
+            />
+          </>
+        )}
       </div>
 
       <div className="bg-zinc-100 rounded-2xl p-4 w-1/3 flex flex-col gap-2">
-        <h3 className="font-bold">
+        <div
+          className="flex justify-between cursor-pointer"
+          onClick={toggleOpen}
+        >
+          <h3 className="font-bold">
+            <FontAwesomeIcon
+              icon={faChartLine}
+              className="mr-2 text-zinc-600"
+            />
+            Total Sales:
+          </h3>
           <FontAwesomeIcon
-            icon={faChartLine}
-            className="mr-2 text-zinc-600"
+            icon={isOpen.inventory ? faChevronUp : faChevronDown}
+            className="text-zinc-400"
           />
-          Total Sales:
-        </h3>
-        <p className="text-3xl font-semibold text-zinc-600">
-          {data.totalSales.toLocaleString("en-US", {
-            style: "currency",
-            currency: "USD",
-          })}
-        </p>
-        <ReactApexChart
-          options={sparkOptions}
-          series={[{ data: data.salesSpark }]}
-          type="area"
-          height={'80px'}
-        />
+        </div>
+        {isOpen.sales && (
+          <>
+            <p className="text-3xl font-semibold text-zinc-600">
+              {data.totalSales.toLocaleString("en-US", {
+                style: "currency",
+                currency: "USD",
+              })}
+            </p>
+            <ReactApexChart
+              options={sparkOptions}
+              series={[{ data: data.salesSpark }]}
+              type="area"
+              height={"80px"}
+            />
+          </>
+        )}
       </div>
     </div>
   );
